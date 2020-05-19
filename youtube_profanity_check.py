7b68e8a989adaccba214df09b67fd23ae4e0b0bd
Aaditya Tile <adi01tile@gmail.com>
2020-05-19 21:23:55 +0530

    sentiment and profanity analysis

--- youtube_profanity_check.py
+++ youtube_profanity_check.py
@@ -2,17 +2,20 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By 
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from textblob import TextBlob
+from better_profanity import profanity
 from time import sleep
 import os
 
 PATH = 'C:\Program Files (x86)\chromedriver.exe'  #provide path to chromedriver.exe here.
 
 class ProfanityBot:
-	def __init__(self, channel_url, vid_cnt):
+	def __init__(self, channel_url, vid_cnt, del_transcript= True):
 		self.driver = webdriver.Chrome(PATH)
 		self.channel_url = channel_url
 		self.channel_name = ''
 		self.vid_cnt = vid_cnt  #the number of videos to be fetched from each channel.
+		self.del_transcript = del_transcript
 
 	def fetch_transcripts(self):
 		self.driver.get(self.channel_url+"/videos")  #to jump on videos section.
@@ -20,6 +23,7 @@ class ProfanityBot:
 		links = WebDriverWait(self.driver, 10).until(EC.presence_of_all_elements_located((By.ID, "video-title")))
 		print("Started process..")
 		print("Scraping transcripts from {} channel...".format(self.channel_name))
+		toggle = True
 		try:
 			count = 0
 			links_list = []
@@ -36,9 +40,13 @@ class ProfanityBot:
 				try:
 					transcript_btn = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "ytd-popup-container.style-scope.ytd-app:nth-child(13) iron-dropdown.style-scope.ytd-popup-container:nth-child(1) div.style-scope.iron-dropdown ytd-menu-popup-renderer.style-scope.ytd-popup-container paper-listbox.style-scope.ytd-menu-popup-renderer:nth-child(1) > ytd-menu-service-item-renderer.style-scope.ytd-menu-popup-renderer:nth-child(2)")))
 					transcript_btn.click()
-					# # to toggle timestamp(just reomve 2 lines from below if you you wanna keep timestamp)
-					# WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, "//ytd-menu-renderer[@class='style-scope ytd-engagement-panel-title-header-renderer']//button[@id='button']"))).click()
-					# WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, "//ytd-menu-service-item-renderer[@class='style-scope ytd-menu-popup-renderer']"))).click()
+					# to toggle timestamp we just need to run this once.
+					sleep(2)
+					if toggle:
+						WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, "//ytd-menu-renderer[@class='style-scope ytd-engagement-panel-title-header-renderer']//button[@id='button']"))).click()
+						WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, "//ytd-menu-service-item-renderer[@class='style-scope ytd-menu-popup-renderer']"))).click()
+						toggle = False  #once toggled, no need to toggle again.
+
 					transcript_text = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "ytd-transcript-body-renderer"))).text
 					count += 1
 				except:
@@ -59,18 +67,53 @@ class ProfanityBot:
 		finally:
 			self.driver.quit()
 			print(f'Done scraping transcripts of {self.vid_cnt} videos from {self.channel_name} channel.')
+			print('---------------------------------------------------------------------------------')
+			#sending scraped transcript for sentiment and profanity analysis.
+			self.profanity_analysis(filepath)  
+			self.sentiment_analysis(filepath)
+			# deleting transcripts after use.
+			if self.del_transcript == True:
+				os.remove(filepath)
 
+	# Profanity analysis
+	def profanity_analysis(self, filepath):
+		curse_word_count = 0
+		total_word_count = 0
+		curse_words = []
+		with open(filepath, 'r') as file:
+			text = file.read()
+			sentences = text.split('\n')
+			for sentence in sentences:
+				word_list = sentence.split(' ')
+				for word in word_list:
+					if profanity.contains_profanity(word):  #returns True if word is curse/swear/profane word.
+						curse_words.append(word)
+						curse_word_count += 1
+					total_word_count += 1 
+		profanity_percentage = (curse_word_count/total_word_count) * 100
+		print(f'Curse words used: {curse_words}')
+		print(f'Profanity/curse/swear percentage: {round(profanity_percentage, 5)}%') 
 
-blob = ProfanityBot('https://www.youtube.com/channel/UC4JX40jDee_tINbkjycV4Sg', 7)
-blob.fetch_transcripts()
+	# Sentiment analysis
+	def sentiment_analysis(self, filepath):
+		pos_count = 0
+		neg_count = 0
+		total_count = 0
+		with open(filepath, 'r') as file:
+			text = file.read()
+			subjectivity = TextBlob(text).subjectivity
+			sentence_li = text.split('\n')
+			for sentence in sentence_li:
+				blob = TextBlob(sentence)
+				if blob.sentiment.polarity > 0:
+					pos_count += 1
+				elif blob.sentiment.polarity < 0:
+					neg_count += 1
+				total_count += 1
+		pos_sentiment = (pos_count/total_count) * 100
+		neg_sentiment = (neg_count/total_count) * 100
+		print(f'Positive sentiment: {round(pos_sentiment, 4)}%\nNegative sentiment: {round(neg_sentiment, 4)}%\nSubjectivity: {round(subjectivity, 4)}')
 
-
-
-
-# from textblob import TextBlob
-# curse_words = ["fuck", "fuck you", "fuck off", "shit", "piss off", "dick head", "asshole", "son of a bitch", "bastard", "bitch", "damn", "cunt", "bloody hell", "bollocks", "bugger", "rubbish", "shag", "twat", "bloody oath", "fuck me"]
-# line = "That car is mine! I loved that car. You better keep your fuck out of that car. Fuck off you jerk! I hate you bitch"
-# blob = TextBlob(line)
-# print(blob.sentiment)
-# print(blob.words.count("i"))
+obj = ProfanityBot('https://www.youtube.com/user/codyko69', 2)
+obj.fetch_transcripts()
 
